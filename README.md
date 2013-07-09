aedaf17c054e30794f30b5d0255140549423e70c
dark_key3 <alexanderdarkkey@gmail.com>
2013-07-09 17:14:12 +0400

    fixed README.md

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ icerlpcap
 =========
 
 testing cross-platform pcap in erlang (ugly prototype of multithreaded port scanner in erlang)
+currently supports erlang/x64 on win7-8 and WinPcap 4.1.2
--- erpcap.erl
+++ erpcap.erl
@@ -1,24 +1,28 @@
-%%% erlang sniffer
-%%% original author: xumingyong (http://xumingyong.iteye.com/blog/586743)  
-  
--module(erpcap).  
--on_load(on_load/0).  
-  
--export([lookup/0, opendevice/1, capture/0, loop/1]).  
-  
-on_load() ->  
-    ok = erlang:load_nif("./erpcap", 0),  
-    true.  
-  
-lookup() ->  
-    error.  
-  
-opendevice(_Interface) ->  
-    error.  
-  
-capture() ->  
-    error.  
-  
+-module(erpcap).
+-export([foo/1, bar/1, lookup/0, lookup_device_name/1, opendevice/1, capture/0, loop/1]).
+-on_load(init/0).
+
+init() ->
+    ok = erlang:load_nif("x64/Debug/erpcap", 0).
+
+
+foo(_X) ->
+    exit(nif_library_not_loaded).
+bar(_Y) ->
+    exit(nif_library_not_loaded).
+
+lookup() ->
+    exit(nif_library_not_loaded).
+
+lookup_device_name(_N) ->
+    exit(nif_library_not_loaded).
+
+opendevice(_Device) ->
+    exit(nif_library_not_loaded).
+
+capture() ->
+	exit(nif_library_not_loaded).
+
 loop(0) ->  
     ok;  
 loop(Count) ->  
